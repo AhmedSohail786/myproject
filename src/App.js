c55ba7e224bb990e9c6b40d919458c73c3cb48db
Your Name <youremail@example.com>
2023-08-23 13:55:57 +0500

    done

---
+++ src/App.js
@@ -0,0 +1,359 @@
+// import logo from './logo.svg';
+import './App.css';
+import img1 from './images/img1.jpg';
+import img2 from './images/img-2.jpg';
+import img3 from './images/img-3.jpg';
+import img4 from './images/img-4.jpg';
+import img5 from './images/img5.jpg';
+import img6 from './images/img6.jpg';
+import img7 from './images/img7.jpg';
+import img8 from './images/img8.svg';
+import img11 from './images/img-11.jpg';
+import img12 from './images/img-12.jpg';
+import img13 from './images/img-13.jpg';
+// import img10 from './images/img10.jpg';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
+         <center>
+         <h1 id='do'>DORSIN</h1>
+        <nav id='nav'>
+        
+          <ul id='ul'>
+          
+          {/* <h1> <li>DORSIN</li></h1> */}
+            <li id='li'>Home</li>
+            <li id='li'>Services</li>
+            <li id='li'>Features</li>
+            <li id='li'>Pricing</li>
+            <li id='li'>Team</li>
+            <li id='li'>Blog</li>
+            <li id='li'>Contact</li>
+            {/* <h4><li>Try it Free</li></h4> */}
+          </ul>
+        </nav>
+        </center>
+        <div id='firsta'>
+          <div id='boxa'>
+        <h1 id='text'>
+        We help startups launch <br></br> their products
+        </h1>
+        <p id='text1'>Etiam sed.Interdum consequat proin vestibulum class at.</p>
+        </div>
+        </div>
+        <div id='seconda'>
+          <div>
+          <h2 id='text2'>
+          A digital web design studio<br></br> creating modern & engaging<br></br> online experiences
+          </h2>
+          <br></br>
+          <h5 id='text2'>Separated they live in Bookmarksgrove right at the<br></br> coast of the Semantics, a large language ocean. A small<br></br> river named Duden flows by their place and supplies it<br></br> with the necessary regelialia.</h5>
+          <ul>
+            <li>We put a lot of effort in design.</li>
+            <li>The most important ingredient of successful website.</li>
+            <li>Sed ut perspiciatis unde omnis iste natus error sit.</li>
+            <li>Sed ut perspiciatis unde omnis iste natus error sit.</li>
+          </ul>
+        <button id='but'>Learn MORE</button>
+          </div>
+          <div>
+          <img id='img8' src={img8} alt="img8" width="98%" />
+          </div>
+        </div>
+       <br></br>
+       <div id='bg1'>
+       <center>
+        
+        <div id='boxbg1'>
+          <h1>
+          <p id='text11'> BUILD YOUR DREAM WEBSITE TODAY </p></h1>
+          <h3>
+         <p id='text11'>
+            <br></br>
+            But nothing the copy said could convince her and so it didn’t take long until a few insidious <br></br>
+             Copy Writers ambushed her.
+
+
+          
+            </p>
+            </h3>
+            <button color='red'>view plan and pricing</button>
+        
+      
+        </div>
+        </center>
+        </div>
+       <div id='only'>
+          <center><h1><p>OUR PRICING</p></h1>
+       
+       <hr color='red' width="10%"></hr>
+       <h4>
+       <p>Call to action pricing table is really crucial to your for your business website. 
+        <br></br>
+        Make your bids stand-out with amazing options.</p></h4>
+       </center>
+        </div>
+       <center>
+        <div id='parent6'>
+          <div id='box6'>
+<p>Economy</p>
+<h1>$9.90</h1>
+Billing per month <br></br><br></br>
+
+<hr width="50%"></hr>
+<p>Bandwidth:1 GB <br></br><br></br>
+Onlinespace: 50MB <br></br><br></br>
+
+
+Support: No<br></br><br></br>
+
+1 Domain<br></br><br></br>
+
+No Hidden Fees
+<br></br>
+<button  id='button'>join now</button>
+</p>
+
+          </div>
+          <div id='box6'>
+<p>deluxe</p>
+<h1>$19.90</h1>
+Billing per month <br></br><br></br>
+<hr  width="50%"></hr>
+<p>Bandwidth:10 GB <br></br><br></br>
+Onlinespace: 500MB <br></br><br></br>
+
+
+Support: yes<br></br><br></br>
+
+10 Domain<br></br><br></br>
+
+No Hidden Fees
+<br></br>
+<button  id='button'>join now</button>
+</p>
+</div>
+<div id='box6'>
+
+<p>Ultimate</p>
+<h1>$29.90</h1>
+Billing per month <br></br><br></br>
+<hr  width="50%"></hr>
+<p>Bandwidth:100 GB <br></br><br></br>
+Onlinespace: 2GB <br></br><br></br>
+
+
+Support: yes<br></br><br></br>
+
+unlimited<br></br><br></br>
+
+No Hidden Fees
+<br></br>
+<button id='button'>join now</button>
+</p>
+</div>
+
+        </div>
+        </center>
+        <p id='first'>
+          <center><h3>BEHIND THE PEOPLE</h3></center>
+          <br></br>
+          
+          <center>
+         <h5> It is a long established fact that create category leading brand experiences a reader will be distracted by <br></br> the readable content of a page when looking at its layout.</h5>
+         </center>
+        </p>
+        
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div id='parent1' >
+            <div id='box1'>
+                <img id='child1' src={img1} alt="img1"width="98%" />
+                <p>frank johnson <br></br>
+                  CEO
+                </p>
+                </div>
+                <div id='box1'>
+         <img  id='child1' src={img2} alt="img2" width="98%" /> 
+         <p>elaine clair<br></br>
+                Designer
+                </p>
+         </div>
+         <div id='box1'>
+
+          <img  id='child1' src={img3} alt="img3"width="98%"/>
+          <p>wardon arthur <br></br>
+                  web developer
+                </p>
+          </div>
+          <div id='box1'>
+          <img  id='child1' src={img4} alt="img4"width="98%"/>
+          <p>joshua stemple <br></br>
+                  manager
+                </p>
+          </div>
+      
+          
+          </div>
+      {/* </header> */}
+      <center>
+      <p>
+          <h1>
+        WORK PROCESS
+        </h1>
+        <h4 >In an ideal world this website wouldn’t exist, a client would acknowledge the importance of having web <br></br>copy before the Proin vitae ipsum vel ex finibus semper design starts.</h4>
+        </p>
+        </center>
+        <div id='main'>
+       
+        <center>
+     
+      <p >
+     
+          <h1>
+          WHAT THEY'VE SAID
+        </h1>
+        <h4 >The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question<br></br>Marks and devious pulvinar metus molestie sed Semikoli.</h4>
+        </p>
+        </center>
+        <div id='parent2'>
+        
+        
+        <div id='box2'>
+                <img id='child2' src={img5} alt="img5" />
+                <p>“I feel confident imposing change on myself.<br></br> It's a lot more fun progressing than looking back.<br></br> That's why scelerisque pretium dolor, <br></br>sit amet vehicula erat pelleque need throw curve balls.” <br></br>
+                  
+                </p>
+                </div>
+                <div id='box2'>
+                <img id='child2' src={img6} alt="img5" />
+                <p>“I feel confident imposing change on myself.<br></br> It's a lot more fun progressing than looking back.<br></br> That's why scelerisque pretium dolor, <br></br>sit amet vehicula erat pelleque need throw curve balls.” <br></br>
+                  
+                </p>
+                </div>
+                <div id='box2'>
+                <img id='child2' src={img7} alt="img5" />
+                <p>“I feel confident imposing change on myself.<br></br> It's a lot more fun progressing than looking back.<br></br> That's why scelerisque pretium dolor, <br></br>sit amet vehicula erat pelleque need throw curve balls.” <br></br>
+                  
+                </p>
+                </div>
+                
+
+        </div>
+        </div>    
+      <div id='bg'>
+        <center>
+        
+          <div id='boxbg'>
+            <h1>
+            <p id='text1'>Let's Get Started   </p></h1>
+            <h3>
+           <p id='text1'>
+              <br></br>
+              Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+              <br></br>
+              blind texts.
+              </p>
+              </h3>
+              <button color='red'>get started</button>
+          
+        
+          </div>
+          </center>
+
+        
+        </div>   
+        <div id='sb'>
+        <div id='only'>
+          <center><h1><p>BLOG</p></h1>
+       
+       <hr color='red' width="10%"></hr>
+       <h4>
+       <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean class
+        <br></br>
+        at a euismod mus ipsum vel ex finibus semper luctus quam.</p></h4>
+       </center>
+        </div>
+        
+<div id='parent7'>
+<div id='box7'>
+<img  src={img11} alt="img11"  width="98%"/>
+<p id='likhahua'>
+  UI UX design <br></br>
+  <h4 color='black'>Doing a cross country road trip</h4> <br></br>
+  She packed her seven versalia, put her initial into the <br></br>
+  belt and made herself on the way..
+  <h5 >Read more</h5>
+</p>
+</div>
+<div id='box7'>
+<img  src={img12} alt="img12"  width="98%" />
+<p id='likhahua'>
+Digital Marketing<br></br>
+  <h4 color='black'>New exhibition at our Museum</h4> <br></br>
+
+  Pityful a rethoric question ran over her cheek, then<br></br>
+  she continued her way.
+  <h5 >Read more</h5>
+</p>
+
+</div>
+<div id='box7'>
+<img  src={img13} alt="img13"  width="98%"/>
+<p id='likhahua'>
+Travelling<br></br>
+  <h4 color='black'>New exhibition at our Museum</h4> <br></br>
+
+  Pityful a rethoric question ran over her cheek, then<br></br>
+  she continued her way.
+  <h5 >Read more</h5>
+</p>
+</div>
+</div>
+</div>
+<div id='footer'>
+  <div id='boxfoot'>
+  <h3>DORSIN</h3>
+  Home <br></br>
+About us <br></br>
+Careers <br></br>
+Contact us<br></br>
+</div>
+<div id='boxfoot'>
+  <h3>INFORMATION</h3>
+  terms & conditions <br></br>
+About us <br></br>
+jobs <br></br>
+book marks<br></br>
+</div>
+<div id='boxfoot'>
+  <h3>SUPPORT US</h3>
+  FAQ <br></br>
+Contact us <br></br>
+discussion <br></br>
+
+</div>
+<div id='boxfoot'>
+  <h3>SUBSCRIBE</h3>
+  Subscribe
+In an ideal world this text wouldn’t <br></br>exist, a client would acknowledge the <br></br>importance of having web copy before<br></br> the design starts.
+</div>
+</div>
+<br></br>
+<br></br>
+<div id='last'>
+2019 - 2023 © Dorsin - Themesbrand
+
+
+
+</div>
+    </div>
+ 
+  );
+}
+
+export default App;
